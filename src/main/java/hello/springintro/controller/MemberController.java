c0d2ff91561c4d1741ec406094a26166f1b6ab34
inhalin <inhalin.kim@gmail.com>
2022-07-11 21:26:23 +0900

    자바 코드로 직접 스프링 빈 등록 실습

--- src/main/java/hello/springintro/controller/MemberController.java
+++ src/main/java/hello/springintro/controller/MemberController.java
@@ -9,8 +9,21 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    // DI - 생성자 주입
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+/* DI - 필드 주입
+    @Autowired private MemberService memberService;
+*/
+
+/* DI - setter 주입
+    private MemberService memberService;
+
+    public void setMemberService(MemberService memberService) {
+        this.memberService = memberService;
+    }
+*/
 }
